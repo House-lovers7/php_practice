dae7a9a9877333581a81e00f6e4048687626d2ae
House-lovers7 <taisuke.got@gmail.com>
2021-09-24 09:59:41 +0900

    phpのコード学習

---
+++ sample02.php
@@ -0,0 +1,12 @@
+<?php
+// コメントアウト
+
+/*
+複数行の
+コメントです
+*/
+echo "I'm Japanese";
+echo "I'm \"Japanese\"";
+echo "
+改行を入れて出力します";
+echo "\n改行を\n入れて\n出力します";
---
+++ sample07.php
@@ -0,0 +1,12 @@
+<?php
+$price = 100;
+echo "$price";
+echo " 50 + 50 は　$price です";
+echo "\n";
+echo  '50 + 50 は、', $price, 'です';
+echo "\n";
+echo  '50 + 50 は、' . $price . 'です';
+
+$number = 10;
+echo "\n";
+echo "{$number}th anniversary";
